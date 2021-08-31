a12d131a29ce7f312a644dd34392aae3d07deef6
Hana <momenthana@kakao.com>
2021-08-31 13:38:39 +0900

    Add Home parallax image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,20 @@
 <template>
-  <div style="height: 200vh">Home</div>
+  <v-main>
+    <v-sheet height="100vh" color="#7fefd3">
+      <v-parallax :src="require('@/assets/home.jpg')" style="height: 100vh">
+        <v-container fill-height>
+          <v-layout fill-height align-center>
+            <div style="color: #313131">
+              <h1 class="font-weight-black">입시의 주체를</h1>
+              <h1 class="font-weight-black">학생으로</h1>
+            </div>
+          </v-layout>
+        </v-container>
+      </v-parallax>
+    </v-sheet>
+    <v-sheet height="100vh"></v-sheet>
+    <v-sheet height="273px" color="#f5f4f4"></v-sheet>
+  </v-main>
 </template>
 
 <script lang="ts">
