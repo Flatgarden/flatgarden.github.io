baa52ae47d4cfa1482e2ee5a2431274990e90f6b
Hana <momenthana@kakao.com>
2021-09-02 17:03:13 +0900

    Add Home center card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,6 +15,28 @@
           </v-layout>
         </v-container>
       </v-parallax>
+      <v-row
+        justify="center"
+        no-gutters
+        :style="
+          $vuetify.breakpoint.xsOnly ? 'position: relative; top: -260px' : null
+        "
+      >
+        <v-card style="top: -135px" class="ma-3" width="275px" elevation="10">
+          <v-img height="150px" :src="require('@/assets/hakhak_card.jpg')" />
+          <v-card-title><b>입시친구 학학이</b></v-card-title>
+          <v-card-subtitle class="text-caption">
+            입시 종합 콘텐츠 어플리케이션
+          </v-card-subtitle>
+        </v-card>
+        <v-card style="top: -135px" class="ma-3" width="275px" elevation="10">
+          <v-img height="150px" :src="require('@/assets/studiosha_card.jpg')" />
+          <v-card-title><b>스튜디오 샤</b></v-card-title>
+          <v-card-subtitle class="text-caption">
+            서울대생들의 공부법과 일상을 알 수 있는 유튜브 채널
+          </v-card-subtitle>
+        </v-card>
+      </v-row>
     </v-sheet>
     <v-sheet height="100vh"></v-sheet>
   </v-main>
