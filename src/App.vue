04f658ddbf0b2f413f6dd1780553240e227e8713
Hana <momenthana@kakao.com>
2021-09-13 12:30:07 +0900

    Update youtube card items

--- src/App.vue
+++ src/App.vue
@@ -42,7 +42,7 @@ export default Vue.extend({
             path: "https://www.googleapis.com/youtube/v3/playlistItems",
             params: {
               part: "snippet",
-              playlistId: "PLhcs_k82PxkGjlDfgu68ZI3aYIRRhnfDg",
+              playlistId: "PLpM0VV5-ga-qXmTIqjhhd99DqxZADlr0Z",
               maxResults: 50,
             },
           })
@@ -50,9 +50,30 @@ export default Vue.extend({
         .then(
           (response) => {
             const items = response.result.items
-            this.items.third = items.slice(0, 3)
-            this.items.second = items.slice(3).sort(() => Math.random() - 0.5)
+            this.items.second = items.sort(() => Math.random() - 0.5)
             this.items.first = this.items.second.slice().reverse()
+          },
+          (reason) => {
+            console.log("Error: " + reason.result.error.message)
+          }
+        )
+      gapi.client
+        .init({
+          apiKey: "AIzaSyAKGWz-vDUt9GIagEck0_9dzOM22V6tqdw",
+        })
+        .then(() =>
+          gapi.client.request({
+            path: "https://www.googleapis.com/youtube/v3/playlistItems",
+            params: {
+              part: "snippet",
+              playlistId: "PLpM0VV5-ga-o5N3W0DuMEnLb0XuEJPfZO",
+              maxResults: 3,
+            },
+          })
+        )
+        .then(
+          (response) => {
+            this.items.third = response.result.items
           },
           (reason) => {
             console.log("Error: " + reason.result.error.message)
